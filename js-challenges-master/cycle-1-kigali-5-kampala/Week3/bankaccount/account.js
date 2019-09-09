71a642b692277e859ce89ebe4eb0a1e79354c46f
Cleopatra Kanyunyuzi <kanyunyuzicleo@gmail.com>
2019-09-09 02:41:49 -0700

    account.1

---
+++ js-challenges-master/cycle-1-kigali-5-kampala/Week3/bankaccount/account.js
@@ -0,0 +1,54 @@
+class BankAccount {
+    constructor (name, balance) {
+        // your code here
+        this.name = name;
+      this.balance = balance;
+     
+
+
+    }
+
+    getBalance() {
+        // your code here
+        
+
+    }
+
+    open (){ 
+        // your code here
+        
+          }
+          
+        
+        
+
+    
+
+    deposit ( ) {
+        // your code here
+       
+
+    }
+
+
+    withdraw ( ) {
+        // your code here
+
+
+
+    }
+
+    close(){
+
+        
+
+    }
+
+      
+      }
+      
+      
+      
+      
+      
+    
