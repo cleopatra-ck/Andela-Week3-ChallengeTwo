3b2d448eb7ff63dd9cb74c14d167a5d828119a62
Cleopatra Kanyunyuzi <kanyunyuzicleo@gmail.com>
2019-09-11 00:40:10 -0700

    acc-test

---
+++ js-challenges-master/cycle-1-kigali-5-kampala/Week3/bankaccount/account.test.js
@@ -0,0 +1,18 @@
+// tests here
+        var user1= new BankAccount("Cleo",2000);
+        user1.open();
+        user1.deposit(1000);
+
+
+        var user2 = new BankAccount("Cliio",3000);
+        user2.withdraw();
+        user2.getbalance();
+
+
+        
+        var user3 = new BankAccount("Patra",8000);
+        user3.close();
+
+
+
+        
