68e3591d0711d34c3282c6237c98e648d54191b2
Cleopatra Kanyunyuzi <kanyunyuzicleo@gmail.com>
2019-09-10 18:11:25 -0700

    acc.4

--- js-challenges-master/cycle-1-kigali-5-kampala/Week3/bankaccount/BankAccount/account.js
+++ js-challenges-master/cycle-1-kigali-5-kampala/Week3/bankaccount/BankAccount/account.js
@@ -2,49 +2,56 @@ class BankAccount {
     constructor (name, balance) {
         // your code here
         this.name = name;
-      this.balance = balance;
-     
-
-
-    }
+      this.balance = balance; 
+   }
+    
 
     getBalance() {
         // your code here
-        alert("Your account balance is Ugx" + this.balance);       
-   }
+ if (this.name != null) {
+        return (this.name +"your account balance is Ugx" + this.balance);
+       }  
+       }
 
     open (){ 
         // your code here
-       
-        this.name = name;
-        this.balance = balance;
-       
-          alert(this.name + " is your account name and Ugx" + this.balance + "is your available|")
-        
-        
-        
-        }
-          
-        
-        
-
     
+        var FirstName = prompt("enter your firstName ");
+       var LastName = prompt ("enter your LastName ");
+       var newamount = prompt ("enter minimum deposit");
+       this.name = FirstName + " " + LastName;
+      this.balance = newamount;
+       if (this.name != null && this.balance > 0){
+          return (this.name + " is your account name and Ugx" + this.balance + "is your available balance.");
+        }
+            
+        }
+                
 
-    deposit ( ) {
+    deposit () {
         // your code here
-       
-
+       if (this.name != null){
+              this.balance += newamount;
     }
+    return (this.name + " this is your new balance Ugx" + this.balance);
+  }
+  
 
 
     withdraw ( ) {
         // your code here
-
-
-
+        if (this.name != null && this.balance > newamount){
+    
+          this.balance -= newamount;
     }
+    return (this.name + " this is your new balance Ugx" + this.balance);
+    
+} 
 
-    close(){
+  
+  close(){
+
+
 
         
 
