5e6c2dcb0f03643bdf272348088712e527f714f2
Cleopatra Kanyunyuzi <kanyunyuzicleo@gmail.com>
2019-09-14 06:41:57 -0700

    BankAccount

---
+++ bankaccount/account.js
@@ -0,0 +1,90 @@
+class BankAccount {
+    constructor (name, balance) {
+        // your code here
+        this.name = name;
+      this.balance = balance; 
+   }
+    
+
+    getBalance() {
+        // your code here
+ if (this.name != null) {
+        return  (this.name + " " + "your account balance is Ugx" + " " + this.balance);
+       }  
+       }
+
+    open (minamount){ 
+        // your code here
+    
+       var minamount;
+       this.balance = minamount;
+       if (this.name != null && this.balance > 0){
+          return (this.name + " " + " is your account name and Ugx" + " " + this.balance + "is your available balance.");
+        }
+            
+        }
+                
+
+    deposit (newamount) {
+        // your code here
+        var newamount;
+       if (this.name != null){
+              this.balance += newamount;
+    }
+   return (this.name + " " + " this is your new balance Ugx" + " " + this.balance);
+  }
+  
+
+
+    withdraw (amount ) {
+        // your code here
+        var amount;
+        if (this.name != null && this.balance > amount){
+    
+          this.balance -= amount;
+    }
+    return (this.name + " " + " this is your new balance Ugx" + " " + this.balance);
+    
+} 
+
+  
+  close(name){
+
+if (this.name = name && this.balance == 0){ 
+  
+  return undefined };
+
+        
+   }
+
+      
+      }
+      
+      var cleo= new BankAccount('cleo',1100);
+        cleo.deposit(400);
+
+
+        var Patra= new BankAccount('Patra',600);
+        Patra.withdraw(100);
+      
+        var Pattie= new BankAccount('Pattie',100);
+        Pattie.getBalance();
+
+        
+        var clio = new BankAccount('clio',0);
+        clio.close(0); 
+
+        var newacc = new BankAccount('newacc' ,5000);
+        newacc.open(5000); 
+
+
+       
+      console.log (cleo);
+      console.log (Patra);
+      console.log (Pattie);
+      console.log (clio);
+      console.log (newacc);
+
+       module.exports = BankAccount;
+      
+    
---
+++ bankaccount/account.test.js
@@ -0,0 +1,39 @@
+// tests here
+       
+const BankAccount = require('./BankAccount');
+
+test('Will not accept an empty name field as input', () => {
+    expect(BankAccount(this.name == null)).toBe('invalid input');
+});
+
+
+test('Will not accept name as number input', () => {
+        expect(BankAccount(this.name == Number )).toBe('invalid input');
+    });
+
+
+    test('Will not accept balance as character input', () => {
+        expect(BankAccount(this.balance == (/[a-zA-Z]/gi) )).toBe('invalid input');
+    });
+
+test('Will return a bank account', () => {
+    expect(
+         deposit (name, balance)
+        .toEqual(
+            (Cleo, 2000)))});
+            
+        ;
+
+  test('transactions against a closed account will fail', () => {
+  expect(
+     Close( Cleo, 0)
+         ).toEqual(
+                        (undefined)
+                        );
+
+
+            
+});
+
+
+        
