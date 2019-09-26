2356906f204fb3026aa027a4d369e21fd0952905
Kanyunyuzi Cleopatra <kanyunyuzicleo@gmail.com>
2019-09-26 03:34:36 -0700

    Update account.test.js

--- bankaccount/account.test.js
+++ bankaccount/account.test.js
@@ -39,40 +39,7 @@ test('closed account', () => {
 
 })
     
-const BankAccount = require('./account.js');
 
-test('Will not accept an empty name field as input', () => {
-    expect(BankAccount(this.name == null)).toBe('invalid input');
-});
-
-
-test('Will not accept name as number input', () => {
-        expect(BankAccount(this.name == Number )).toBe('invalid input');
-    });
-
-
-    test('Will not accept balance as character input', () => {
-        expect(BankAccount(this.balance == (/[a-zA-Z]/gi) )).toBe('invalid input');
-    });
-
-test('Will return a bank account', () => {
-    expect(
-         deposit (name, balance)
-        .toEqual(
-            (Cleo, 2000)))});
-            
-        ;
-
-  test('transactions against a closed account will fail', () => {
-  expect(
-     Close( Cleo, 0)
-         ).toEqual(
-                        (undefined)
-                        );
-
-
-            
-});
 
 
         
